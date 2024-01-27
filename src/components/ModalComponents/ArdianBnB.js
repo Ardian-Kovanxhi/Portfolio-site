fbc7f24a4a2cef308ee2dcda1623d2e7ef3dbc28
Ardian-Kovanxhi <akovanxhi@gmail.com>
2024-01-26 20:02:23 -0500

    linked

--- src/components/ModalComponents/ArdianBnB.js
+++ src/components/ModalComponents/ArdianBnB.js
@@ -2,6 +2,7 @@ export default function ArdianBnB() {
     return (
         <>
             ARDIANBNB COMPONENT
+            {/* https://cdn3.vectorstock.com/i/1000x1000/68/42/pixel-open-closed-door-detailed-isolated-vector-22866842.jpg */}
         </>
     )
 }
